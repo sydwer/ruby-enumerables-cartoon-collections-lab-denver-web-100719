f104f5de31b852aa1fcaf19c4ce13b9fdf9d54f4
Sydney Werbach <sydwer@gmail.com>
2019-09-30 06:21:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,7 @@ end
 
 def find_the_cheese(food_food_fabulous_food)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  food_food_fabulous_food. 
+  if food_food_fabulous_food.include? ("cheddar" || "gouda" || "camembert") do
+    
     
 end
