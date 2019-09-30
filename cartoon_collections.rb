9bb796efe69c003031e161efb017e39deabc0267
Sydney Werbach <sydwer@gmail.com>
2019-09-30 06:16:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,5 @@ end
 
 def find_the_cheese(food_food_fabulous_food)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  food_food_fabulous_food.include? (
+  food_food_fabulous_food.include? (|cheddar|
 end
