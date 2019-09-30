9c2c76b88f2626650ebc936abca4bc8c099e84ec
Sydney Werbach <sydwer@gmail.com>
2019-09-30 05:29:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,6 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(attendance_sheet)
+  attendance_sheet.each
+  
 end
 
 def summon_captain_planet# code an argument here
