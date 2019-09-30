e32a5531356dd46561514aa2a9300f6138822de1
Sydney Werbach <sydwer@gmail.com>
2019-09-30 06:27:56 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,5 +15,8 @@ end
 
 def find_the_cheese(food_food_fabulous_food)
   cheese_types = ["cheddar", "gouda", "camembert"]
-  food_food_fabulous_food.select { |item| item.include?("cheddar" || "gouda" || "camembert")}
+  if food_food_fabulous_food.include?("cheddar" || "gouda" || "camembert")}
+    return food_food_fabulous_food.select { |item| item.include?("cheddar" || "gouda" || "camembert")
+  else
+    return "nil"
 end
